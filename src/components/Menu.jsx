eed194211f632b566924574481c9634e56ef3b47
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-03-02 12:41:44 -0800

    Responsive Navbar, smooth section scrolling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,14 @@
 import './Menu.css'
+import { Link } from "react-scroll"
 
 export default function Menu() {
     return (
-        <div>
-            
+        <div className='menu'>
+            <ul>
+                <li><Link to="about" smooth={true} offset={-57} duration="500">About</Link></li>
+                <li><Link to="portfolio" smooth={true} offset={-57} duration="500">Portfolio</Link></li>
+                <li><Link to="contact" smooth={true} offset={-57} duration="500">Contact</Link></li>
+            </ul>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Navbar.css'
-import Menu from './MenuIcon'
+import MenuIcon from './MenuIcon'
+import Menu from './Menu'
 
 export default function Navbar() {
     const [showMenuIcon, setShowMenuIcon] = useState(false)
+    const [screenWdith, setScreenWidth] = useState(window.innerWidth)
+
+    function handleResize() {
+        setScreenWidth(window.innerWidth)
+        if (screenWdith <= 560) {
+            setShowMenuIcon(true)
+        } else {
+            setShowMenuIcon(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [screenWdith])
 
     return (
         <nav className="navbar">
-            {showMenuIcon && <MenuIcon />}
+            {showMenuIcon ? <MenuIcon /> : <Menu />}
         </nav>
     )
 }
